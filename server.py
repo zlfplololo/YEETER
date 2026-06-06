1578e6ee3156a0a5867973613aa9bb1e5c937b6c
zlfplololo <artemkutasenko@gmail.com>
2026-06-06 13:17:08 +0300

    added settings

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import socket
 import re
 import json
 import sqlite3
+import bcrypt
+import os
 # 1. Create a socket (AF_INET = IPv4, SOCK_STREAM = TCP)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -100,6 +102,17 @@ while True:
                     ).encode("utf-8")
                     response = headers + content
                     client_socket.sendall(response)
+            elif data.decode().split('\n')[0].split(' ')[1] == "/PFP's":
+                response_body = json.dumps(os.listdir("assets/PFP's")).encode("utf-8")
+                headers = (
+                    "HTTP/1.1 200 OK\n"
+                    "Content-Type: application/json\n"
+                    f"Content-Length: {len(response_body)}\n"
+                    "Access-Control-Allow-Origin: *\n"
+                    "Connection: close\n\n"
+                ).encode("utf-8")
+                response = headers + response_body
+                client_socket.sendall(response)
             elif re.match(r"(\/assets\/PFP's\/.+)",data.decode().split('\n')[0].split(' ')[1]):
                 try:
                     with open(data.decode().split('\n')[0].split(' ')[1][1:], "rb") as f:
@@ -137,16 +150,72 @@ while True:
                     ).encode("utf-8")
                     response = headers + content
                     client_socket.sendall(response)
-            elif re.match(r"\/userdata\/.*",data.decode().split('\n')[0].split(' ')[1]):
+            elif re.match(r"(\/post\?id=.*)",data.decode().split('\n')[0].split(' ')[1]):
+                with open("post.html", "rb") as f:
+                    content = f.read()
+                    headers = (
+                        "HTTP/1.1 200 OK\n"
+                        "Content-Type: text/html; charset=utf-8\n"
+                        f"Content-Length: {len(content)}\n"
+                        "Connection: close\n"
+                        "\n"
+                    ).encode("utf-8")
+                    response = headers + content
+                    client_socket.sendall(response)
+            elif re.match(r"(\/posts\/\d+)",data.decode().split('\n')[0].split(' ')[1]):
                 table = sqlite3.connect('data.db')
                 cursor = table.cursor()
-                cursor.execute("SELECT pfp, name, at, description FROM users WHERE at = ?", (data.decode().split('\n')[0].split(' ')[1].split("/")[2],))
-                resulte = cursor.fetchall()
-                cursor.execute('SELECT following FROM users')
-                following = len([i for i in cursor.fetchall() if data.decode().split('\n')[0].split(' ')[1].split("/")[2] in json.loads(i[0])])
-                cursor.execute('SELECT * FROM posts WHERE at = ?', (data.decode().split('\n')[0].split(' ')[1].split("/")[2],))
+                cursor.execute('SELECT at, content FROM posts WHERE id = ?', (data.decode().split('\n')[0].split(' ')[1].split("/")[2],))
                 posts = cursor.fetchall()
-                response_body = json.dumps(list(resulte[0])+[following]+[posts]).encode("utf-8")
+                response_body = json.dumps(list(posts[0])).encode("utf-8")
+                headers = (
+                    "HTTP/1.1 200 OK\n"
+                    "Content-Type: application/json\n"
+                    f"Content-Length: {len(response_body)}\n"
+                    "Access-Control-Allow-Origin: *\n"
+                    "Connection: close\n\n"
+                ).encode("utf-8")
+                response = headers + response_body
+                client_socket.sendall(response)
+            elif re.match(r"\/userdata\/.*",data.decode().split('\n')[0].split(' ')[1]):
+                try:
+                    table = sqlite3.connect('data.db')
+                    cursor = table.cursor()
+                    cursor.execute("SELECT pfp, name, at, description FROM users WHERE at = ?", (data.decode().split('\n')[0].split(' ')[1].split("/")[2],))
+                    resulte = cursor.fetchall()
+                    cursor.execute('SELECT following FROM users')
+                    followers = len([i for i in cursor.fetchall() if data.decode().split('\n')[0].split(' ')[1].split("/")[2] in json.loads(i[0])])
+                    cursor.execute('SELECT * FROM posts WHERE at = ?', (data.decode().split('\n')[0].split(' ')[1].split("/")[2],))
+                    posts = cursor.fetchall()
+                    cursor.execute('SELECT following FROM users WHERE at = ?', (data.decode().split('\n')[0].split(' ')[1].split("/")[2],))
+                    following = cursor.fetchall()
+                    response_body = json.dumps(list(resulte[0])+[followers]+[posts]+[json.loads(following[0][0])]).encode("utf-8")
+                    headers = (
+                        "HTTP/1.1 200 OK\n"
+                        "Content-Type: application/json\n"
+                        f"Content-Length: {len(response_body)}\n"
+                        "Access-Control-Allow-Origin: *\n"
+                        "Connection: close\n\n"
+                    ).encode("utf-8")
+                    response = headers + response_body
+                    client_socket.sendall(response)
+                except IndexError:
+                    with open("error.html", "rb") as f:
+                        content = f.read()
+                        headers = (
+                            "HTTP/1.1 200 OK\n"
+                            "Content-Type: text/html; charset=utf-8\n"
+                            f"Content-Length: {len(content)}\n"
+                            "Connection: close\n"
+                            "\n"
+                        ).encode("utf-8")
+                        response = headers + content
+                        client_socket.sendall(response)
+            elif re.match(r"\/usershort\/.*",data.decode().split('\n')[0].split(' ')[1]):
+                table = sqlite3.connect('data.db')
+                cursor = table.cursor()
+                cursor.execute("SELECT pfp, name FROM users WHERE at = ?", (data.decode().split('\n')[0].split(' ')[1].split("/")[2],))
+                response_body = json.dumps(list(resulte[0])).encode("utf-8")
                 headers = (
                     "HTTP/1.1 200 OK\n"
                     "Content-Type: application/json\n"
@@ -185,9 +254,11 @@ while True:
             action = json.loads(data.decode().split('\r\n\r\n')[1])
             if action["action"] == "account/create":
                 try:
+                    with open("assets/bell.json","r") as f:
+                        counters = json.loads(f.readlines()[0])[1].encode("utf-8")
                     table = sqlite3.connect('data.db')
                     cursor = table.cursor()
-                    cursor.execute("INSERT INTO users (at, name, password, following, pfp, description, notifications, notificated) VALUES (?,?,?,?,?,?,?,?)", (action['name'], action['name'], action['password'], "[]", "standart.png", None, "[]", "[]"))
+                    cursor.execute("INSERT INTO users (at, name, password, following, pfp, description, notifications, notificated) VALUES (?,?,?,?,?,?,?,?)", (action['name'], action['name'], bcrypt.hashpw(action['password'].encode("utf-8"), counters), "[]", "standart.png", None, "[]", "[]"))
                     table.commit()
                     
                     response_body = {"status": 1, "error": "None"}
@@ -217,6 +288,8 @@ while True:
                         client_socket.close()
                         
             if action["action"] == "account/login":
+                with open("assets/bell.json","r") as f:
+                    counters = json.loads(f.readlines()[0])[1].encode("utf-8")
                 table = sqlite3.connect('data.db')
                 cursor = table.cursor()
                 cursor.execute("SELECT password FROM users WHERE at = ?", (action["name"],))
@@ -235,10 +308,9 @@ while True:
                     client_socket.close()
                     continue
                     
-                print(resulte)
                 table.commit()
                 
-                if action["password"] == resulte[0][0]:
+                if bcrypt.hashpw(action["password"].encode("utf-8"), counters) == resulte[0][0]:
                     response_body = {"status": 1, "error": "None"}
                     response_body = json.dumps(response_body).encode("utf-8")
                     response = (
@@ -262,7 +334,111 @@ while True:
                         "Connection: close\n\n"
                     ).encode("utf-8")
                     client_socket.sendall(response + response_body.encode("utf-8"))
-                    client_socket.close()                
+            if action["action"] == "post/post":
+                table = sqlite3.connect('data.db')
+                cursor = table.cursor()
+                cursor.execute("INSERT INTO posts (at, content) VALUES (?,?)", (action["AT"],action["content"]))
+                table.commit()
+                response_body = {"status": 1, "error": "None"}
+                response_body = json.dumps(response_body)
+                response = (
+                    "HTTP/1.1 200 OK\n"
+                    "Content-Type: application/json\n"
+                    f"Content-Length: {len(response_body)}\n"
+                    "Access-Control-Allow-Origin: *\n"
+                    "Connection: close\n\n"
+                ).encode("utf-8")
+                client_socket.sendall(response + response_body.encode("utf-8"))
+                client_socket.close()
+                continue
+            if action['action'] == "user/follow":
+                table = sqlite3.connect('data.db')
+                cursor = table.cursor()
+                cursor.execute("SELECT following FROM users WHERE at = ?", (action["AT"],))
+                result = json.loads(cursor.fetchall()[0][0])
+                if action["who"] in result:
+                    result.remove(action["who"])
+                else:
+                    result.append(action["who"])
+                cursor.execute("UPDATE users SET following = ? WHERE at = ?", (json.dumps(result), action["AT"]))
+                table.commit()
+                response_body = {"status": 1, "error": "None"}
+                response_body = json.dumps(response_body)
+                response = (
+                    "HTTP/1.1 200 OK\n"
+                    "Content-Type: application/json\n"
+                    f"Content-Length: {len(response_body)}\n"
+                    "Access-Control-Allow-Origin: *\n"
+                    "Connection: close\n\n"
+                ).encode("utf-8")
+                client_socket.sendall(response + response_body.encode("utf-8"))
+                client_socket.close()
+                continue
+            if action['action'] == "post/delete":
+                table = sqlite3.connect('data.db')
+                cursor = table.cursor()
+                cursor.execute("DELETE FROM posts WHERE id = ?", (action["id"],))
+                table.commit()
+                if cursor.rowcount == 0:
+                    response_body = {"status": 0, "error": "n/p"}
+                    response_body = json.dumps(response_body)
+                    response = (
+                        "HTTP/1.1 200 OK\n"
+                        "Content-Type: application/json\n"
+                        f"Content-Length: {len(response_body)}\n"
+                        "Access-Control-Allow-Origin: *\n"
+                        "Connection: close\n\n"
+                    ).encode("utf-8")
+                    client_socket.sendall(response + response_body.encode("utf-8"))
+                    client_socket.close()
+                    continue
+                else:
+                    response_body = {"status": 1, "error": "None"}
+                    response_body = json.dumps(response_body)
+                    response = (
+                        "HTTP/1.1 200 OK\n"
+                        "Content-Type: application/json\n"
+                        f"Content-Length: {len(response_body)}\n"
+                        "Access-Control-Allow-Origin: *\n"
+                        "Connection: close\n\n"
+                    ).encode("utf-8")
+                    client_socket.sendall(response + response_body.encode("utf-8"))
+                    client_socket.close()
+                    continue
+            if action["action"] == "post/edit":
+                table = sqlite3.connect('data.db')
+                cursor = table.cursor()
+                cursor.execute("UPDATE posts SET content = ? WHERE id = ?", (action["content"],action["id"]))
+                table.commit()
+                response_body = {"status": 1, "error": "None"}
+                response_body = json.dumps(response_body)
+                response = (
+                    "HTTP/1.1 200 OK\n"
+                    "Content-Type: application/json\n"
+                    f"Content-Length: {len(response_body)}\n"
+                    "Access-Control-Allow-Origin: *\n"
+                    "Connection: close\n\n"
+                ).encode("utf-8")
+                client_socket.sendall(response + response_body.encode("utf-8"))
+                client_socket.close()
+                continue
+            if action["action"] == "user/update status":
+                table = sqlite3.connect('data.db')
+                cursor = table.cursor()
+                cursor.execute("UPDATE users SET pfp = ?, name = ?, description = ? WHERE at = ?", (action["pfp"],action["name"], action["description"], action["at"]))
+                table.commit()
+                response_body = {"status": 1, "error": "None"}
+                response_body = json.dumps(response_body)
+                response = (
+                    "HTTP/1.1 200 OK\n"
+                    "Content-Type: application/json\n"
+                    f"Content-Length: {len(response_body)}\n"
+                    "Access-Control-Allow-Origin: *\n"
+                    "Connection: close\n\n"
+                ).encode("utf-8")
+                client_socket.sendall(response + response_body.encode("utf-8"))
+                client_socket.close()
+                continue
                     
         # 6. Close the connection
         client_socket.close()
